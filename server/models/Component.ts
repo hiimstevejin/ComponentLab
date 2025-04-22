beb4ef90c90fc556ceef2de13f127fcd4c3a96c4
hiimstevejin <jingle5695@gmail.com>
2025-04-22 15:01:15 -0400

    feat(models): define Component mongoose schema

---
+++ server/models/Component.ts
@@ -0,0 +1,56 @@
+import { Document, Schema, model, Types } from "mongoose";
+
+interface CodeFile {
+  name: string;       //  e.g., "FancyButton.jsx "
+  path: string;       //  e.g., "src/components"
+  language: string;   //  e.g., "jsx", "css"
+  content: string;    //  full source code 
+}
+
+interface PropExample {
+  label: string;      //  e.g., "Click Me" 
+}
+
+export interface IComponent extends Document {
+  _id: Types.ObjectId;
+  name: string;
+  description: string;
+  files: CodeFile[];
+  propsExample: PropExample;
+  createdBy: Types.ObjectId;  // refer to User _id
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const componentSchema = new Schema<IComponent>(
+  {
+    _id: {
+      type: Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    name: { type: String, required: true },
+    description: { type: String },
+    files: [
+      {
+        name: { type: String, required: true },
+        path: { type: String, required: true },
+        language: { type: String, required: true },
+        content: { type: String, required: true },
+      }
+    ],
+    propsExample: {
+      label: { type: String, required: true },
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Component = model<IComponent>("Component", componentSchema);
+export default Component;
