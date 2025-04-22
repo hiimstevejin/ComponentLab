fc8c909ecc939e42392cac71fbaed598b414f81e
hiimstevejin <jingle5695@gmail.com>
2025-04-22 02:41:54 -0400

    feat(models): define User mongoose schema

---
+++ server/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Schema, model, Types } from "mongoose";
+
+export interface IUser extends Document {
+  _id: Types.ObjectId;
+  username: string;
+  email: string;
+  avatarUrl: string;
+  joinedAt: Date;
+}
+
+const userSchema = new Schema<IUser>({
+  _id: {
+    type: Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  avatarUrl: String,
+  joinedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = model<IUser>("User", userSchema);
+export default User;
