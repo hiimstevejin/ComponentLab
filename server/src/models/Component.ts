c14aa07fad7a78c6bc67731d8fc5b8c117e73f45
hiimstevejin <jingle5695@gmail.com>
2025-04-27 03:57:29 -0400

    refactor(models): change type of id into Schema.Types.ObjectId

---
+++ server/src/models/Component.ts
@@ -0,0 +1,58 @@
+import { Document, Schema, model, Types } from "mongoose";
+
+interface CodeFile {
+  name: string;       //  e.g., "FancyButton.jsx "
+  path: string;       //  e.g., "src/components"
+  language: string;   //  e.g., "jsx", "css"
+  content: string;    //  full source code 
+}
+
+interface PropExample {
+  label: string;      //  e.g., "Click Me" 
+}
+
+export interface IComponent extends Document {
+  _id: Types.ObjectId;
+  name: string;
+  view: number;
+  description: string;
+  files: CodeFile[];
+  propsExample: PropExample;
+  createdBy: Types.ObjectId;  // refer to User _id
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const componentSchema = new Schema<IComponent>(
+  {
+    _id: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    name: { type: String, required: true },
+    view: {type:Number, default: 0},
+    description: { type: String },
+    files: [
+      {
+        name: { type: String, required: true },
+        path: { type: String, required: true },
+        language: { type: String, required: true },
+        content: { type: String, required: true },
+      }
+    ],
+    propsExample: {
+      label: { type: String, required: true },
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Component = model<IComponent>("Component", componentSchema);
+export default Component;
---
+++ server/src/models/Favorite.ts
@@ -0,0 +1,32 @@
+import { Document, Schema, model, Types } from "mongoose";
+
+export interface IFavorite extends Document {
+  _id: Types.ObjectId;
+  userId: Types.ObjectId;       //refer to user_id
+  componentId: Types.ObjectId;  //refer to component_id
+  favoritedAt: Date;
+}
+
+const favoriteSchema = new Schema<IFavorite>({
+  _id: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  componentId: {
+    type: Schema.Types.ObjectId,
+    ref: "Component",
+    required: true,
+  },
+  favoritedAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const Favorite = model<IFavorite>("Favorite", favoriteSchema);
+export default Favorite;
