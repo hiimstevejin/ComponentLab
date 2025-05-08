9994817e01ab7c36b7c68379d98a59fe8e1ca12f
hiimstevejin <jingle5695@gmail.com>
2025-05-08 18:03:23 -0400

    feat(routes): define componentRoutes with DELETE /api/components/:id

--- server/src/routes/componentRoutes.ts
+++ server/src/routes/componentRoutes.ts
@@ -1,17 +1,21 @@
 import express, { RequestHandler } from "express";
 import {
   createComponent,
+  deleteComponentById,
   getAllComponents,
   getComponentById,
 } from "../controllers/componentController.js";
 
 const router = express.Router();
 
-// POST /api/components -> Create a new component
-router.post("/", createComponent as RequestHandler);
+// POST /api/components 
+// GET /api/components?page=1&limit=20 
+// GET /api/components/680f5617cd4c1f69c459a098 
+// DELETE /api/components/680f5617cd4c1f69c459a098 
 
-// GET /api/components?page=1&limit=20 -> Get paginated list of components
+router.post("/", createComponent as RequestHandler);
 router.get("/", getAllComponents as RequestHandler);
 router.get("/:id", getComponentById as RequestHandler);
+router.delete("/:id", deleteComponentById as RequestHandler);
 
 export default router;
